6c8199b03ddce836a5e98333141007059d67c701
Melker Narikka <melker.narikka@helsinki.fi>
2017-11-17 23:56:52 +0200

    Lisää mallipohja ja alustava käyttäjämalli

---
+++ app/model.php
@@ -0,0 +1,53 @@
+<?php
+
+class Model {
+	function __construct($attr) {
+		foreach ($attr as $k => $v) {
+			if (property_exists($this, $k)) {
+				$this->{$k} = $v;
+			}
+		}
+	}
+
+	function errors() {
+		$ret = array();
+
+		foreach ($this->validators as $v) {
+			$r = $v();
+			if ($r !== true) {
+				$ret[] = $r;
+			}
+		}
+
+		return $ret;
+	}
+
+	protected function _all() {
+		$db = Database::conn();
+		$q = $db->prepare("SELECT * FROM " . static::class);
+		$q->execute();
+
+		$rows = $q->fetchAll(PDO::FETCH_ASSOC);
+		$ret = array();
+
+		foreach ($rows as $row) {
+			$ret[] = new static($row);
+		}
+
+		return $ret;
+	}
+
+	protected function _find($id) {
+		$db = Database::conn();
+		$q = $db->prepare("SELECT * FROM " . static::class . " WHERE id = :id LIMIT 1");
+		$q->bindValue(":id", $id, PDO::PARAM_INT);
+		$q->execute();
+
+		$row = $q->fetch(PDO::FETCH_ASSOC);
+		if (!$row) {
+			return null;
+		}
+
+		return new static($row);
+	}
+}
---
+++ app/models/Account.php
@@ -0,0 +1,37 @@
+<?php
+
+class Account extends Model {
+	public $id;
+	public $nick;
+	public $password;
+	public $admin;
+
+	function __construct($attr) {
+		parent::__construct($attr);
+	}
+
+	function all() {
+		return _all();
+	}
+
+	function find($id) {
+		return _find($id);
+	}
+
+	function save() {
+		$db = Database::conn();
+		$q = $db->prepare("INSERT INTO Account (nick, password, admin) VALUES (:nick, :password, :admin) RETURNING id");
+		$q->bindValue(":nick", $this->nick, PDO::PARAM_STR);
+		$q->bindValue(":password", $this->password, PDO::PARAM_STR);
+		$q->bindValue(":admin", $this->admin, PDO::PARAM_BOOL);
+		$q->execute();
+
+		$row = $q->fetch(PDO::FETCH_ASSOC);
+		if (!$row) {
+			return null;
+		}
+
+		$this->id = $row["id"];
+		return $this;
+	}
+}
