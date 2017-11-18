f59293b31b77cec055918bcb3ba08a3dca09c059
Melker Narikka <melker.narikka@helsinki.fi>
2017-11-18 04:19:39 +0200

    Taulun rakenteen säätämistä

--- app/views/accounts/list.php
+++ app/views/accounts/list.php
@@ -3,20 +3,24 @@ $this->exportParam("title", "Käyttäjät");
 ?>
 <h1>Käyttäjät</h1>
 <table>
-	<tr>
-		<th>Nimimerkki</th>
-		<th>Ylläpitäjä?</th>
-		<th><a href="<?= BASE_DIR ?>/accounts/add">+</a></th>
-	</tr>
+	<thead>
+		<tr>
+			<th>Nimimerkki</th>
+			<th>Ylläpitäjä?</th>
+			<th><a href="<?= BASE_DIR ?>/accounts/add">+</a></th>
+		</tr>
+	</thead>
+	<tbody>
 <?php
 	foreach ($this->params["accounts"] as $a) {
 ?>
-	<tr>
-		<td><?= htmlspecialchars($a->nick) ?></td>
-		<td><?= ($a->admin) ? "On" : "Ei" ?></td>
-		<td><a href="<?= BASE_DIR . "/accounts/{$a->id}" ?>">näytä</a></td>
-	</tr>
+		<tr>
+			<td><?= htmlspecialchars($a->nick) ?></td>
+			<td><?= ($a->admin) ? "On" : "Ei" ?></td>
+			<td><a href="<?= BASE_DIR . "/accounts/{$a->id}" ?>">näytä</a></td>
+		</tr>
 <?php
 	}
 ?>
+	</tbody>
 </table>
