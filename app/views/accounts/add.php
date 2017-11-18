cb9ff96398a161dfceae71ca1a0fcdc7b540dab9
Melker Narikka <melker.narikka@helsinki.fi>
2017-11-18 07:11:22 +0200

    Selitteet kuntoon
    En oikein ymmärrä, miksi p rikkoi nämä.

--- app/views/accounts/add.php
+++ app/views/accounts/add.php
@@ -4,8 +4,8 @@ $this->exportParam("title", "Luo käyttäjä");
 <h1>Luo käyttäjä</h1>
 <p><a href="<?= BASE_DIR ?>/accounts">← Takaisin</a></p>
 <form action="<?= BASE_DIR ?>/accounts/add" method="post">
-	<p><label><p>Nimimerkki</p><input name="nick" type="text" /></label></p>
-	<p><label><p>Salasana</p><input name="password" type="password" /></label></p>
-	<p><label><p>Ylläpitäjä</p><input name="admin" type="checkbox" /></label></p>
+	<p><label>Nimimerkki <br /><input name="nick" type="text" /></label></p>
+	<p><label>Salasana <br /><input name="password" type="password" /></label></p>
+	<p><label>Ylläpitäjä <input name="admin" type="checkbox" /></label></p>
 	<p><input type="submit" value="Luo käyttäjä" /></p>
 </form>
