fa086df0bd16eeda83033e0f035fd567471f4da8
Melker Narikka <melker.narikka@helsinki.fi>
2017-12-05 16:00:05 +0200

    Epäriko testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,59 +1,59 @@
 INSERT INTO Account VALUES (
-	1,
+	DEFAULT, --1,
 	'testi',
 	'$2y$10$Coj81cumngk/errkj8gGD.LQHrluWrwbpGR6vMa6wPfVxCJvQPCS.', -- 'testi'
 	true
 );
 
 INSERT INTO Priority VALUES (
-	1,
+	DEFAULT, --1,
 	1,
 	100,
 	'megatärkeä'
 );
 
 INSERT INTO Priority VALUES (
-	2,
+	DEFAULT, --2,
 	1,
 	-100,
 	'roskis'
 );
 
 INSERT INTO Priority VALUES (
-	3,
+	DEFAULT, --3,
 	1,
 	0,
 	'aika perus'
 );
 
 INSERT INTO Category VALUES (
-	1,
+	DEFAULT, --1,
 	1,
 	'Ostoslista'
 );
 
 INSERT INTO Category VALUES (
-	2,
+	DEFAULT, --2,
 	1,
 	'Katsottavat'
 );
 
 INSERT INTO Task VALUES (
-	1,
+	DEFAULT, --1,
 	1,
 	1,
 	'Täysmaito'
 );
 
 INSERT INTO Task VALUES (
-	2,
+	DEFAULT, --2,
 	1,
 	2,
 	'Riippumatto'
 );
 
 INSERT INTO Task VALUES (
-	3,
+	DEFAULT, --3,
 	1,
 	3,
 	'Vyön rei''itin'
@@ -64,14 +64,14 @@ INSERT INTO TaskCategory VALUES ( 2, 1 );
 INSERT INTO TaskCategory VALUES ( 3, 1 );
 
 INSERT INTO Task VALUES (
-	4,
+	DEFAULT, --4,
 	1,
 	2,
 	'The Village'
 );
 
 INSERT INTO Task VALUES (
-	5,
+	DEFAULT, --5,
 	1,
 	1,
 	'Star Wars Holiday Special'
@@ -81,7 +81,7 @@ INSERT INTO TaskCategory VALUES ( 4, 2 );
 INSERT INTO TaskCategory VALUES ( 5, 2 );
 
 INSERT INTO Task VALUES (
-	6,
+	DEFAULT, --6,
 	1,
 	1,
 	'Lapioi lumet'
