3457d64b7f4f8d8b2bd6ce21d2f3499ddf3e0929
Melker Narikka <melker.narikka@helsinki.fi>
2017-12-27 06:39:40 +0200

    Viilaa listauksia

--- app/views/categories/list.php
+++ app/views/categories/list.php
@@ -3,18 +3,22 @@ $this->exportParam("title", "Luokitukset");
 ?>
 <h1><?= htmlspecialchars($this->exportParams["title"]) ?></h1>
 <table>
-	<tr>
-		<th>Nimi</th>
-		<th><a href="<?= BASE_DIR ?>/categories/add">+</a></th>
-	</tr>
+	<thead>
+		<tr>
+			<th>Nimi</th>
+			<th><a href="<?= BASE_DIR ?>/categories/add">+</a></th>
+		</tr>
+	</thead>
+	<tbody>
 <?php
-foreach ($this->params["categories"] as $p) {
+	foreach ($this->params["categories"] as $p) {
 ?>
-	<tr>
-		<td><?= htmlspecialchars($p->name) ?></td>
-		<td><a href="<?= htmlspecialchars(BASE_DIR . REQ_URL . '/' . $p->id) ?>">näytä</a></td>
-	</tr>
+		<tr>
+			<td><?= htmlspecialchars($p->name) ?></td>
+			<td><a href="<?= htmlspecialchars(BASE_DIR . REQ_URL . '/' . $p->id) ?>">näytä</a></td>
+		</tr>
 <?php
-}
+	}
 ?>
+	</tbody>
 </table>
--- app/views/priorities/list.php
+++ app/views/priorities/list.php
@@ -3,20 +3,24 @@ $this->exportParam("title", "Tärkeysasteet");
 ?>
 <h1>Tärkeysasteet</h1>
 <table>
-	<tr>
-		<th>Nimi</th>
-		<th>Tärkeysaste</th>
-		<th><a href="<?= BASE_DIR ?>/priorities/add">+</a></th>
-	</tr>
+	<thead>
+		<tr>
+			<th>Nimi</th>
+			<th>Tärkeysaste</th>
+			<th><a href="<?= BASE_DIR ?>/priorities/add">+</a></th>
+		</tr>
+	</thead>
+	<tbody>
 <?php
-foreach ($this->params["priorities"] as $p) {
+	foreach ($this->params["priorities"] as $p) {
 ?>
-	<tr>
-		<td><?= htmlspecialchars($p->name) ?></td>
-		<td><?= htmlspecialchars($p->priority) ?></td>
-		<td><a href="<?= htmlspecialchars(BASE_DIR . REQ_URL . '/' . $p->id) ?>">näytä</a></td>
-	</tr>
+		<tr>
+			<td><?= htmlspecialchars($p->name) ?></td>
+			<td><?= htmlspecialchars($p->priority) ?></td>
+			<td><a href="<?= htmlspecialchars(BASE_DIR . REQ_URL . '/' . $p->id) ?>">näytä</a></td>
+		</tr>
 <?php
-}
+	}
 ?>
+	</tbody>
 </table>
